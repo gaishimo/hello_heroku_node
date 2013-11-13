9bf68b6511774999b069eb84470b51e5ef709708
Toru Shimogaisho <gaishmo@gmail.com>
2013-11-13 16:21:08 +0900

    Add cpu_cluster logic

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 var http = require('http');
 var port = process.env.PORT || 5000;
-http.createServer(function(req, res){
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('Hello Heroku Node !');
-}).listen(port);
+var numCPUs = require('os').cpus().length;
+var cluster = require('cluster');
+
+if (cluster.isMaster) {
+  console.log("numCPUs", numCPUs);
+  // Fork workers.
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    console.log('worker ' + worker.process.pid + ' died');
+  });
+}else{
+  http.createServer(function(req, res){
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello Heroku Node !');
+  }).listen(port);
+}
+
